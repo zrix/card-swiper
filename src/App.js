8c7291ba0e9f08e574aeacc9d3d4847889238bc4
Zrix <zrinko.zrilic@gmail.com>
2019-10-30 22:28:59 +0100

    with css only

--- src/App.js
+++ src/App.js
@@ -1,70 +1,99 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 import "./style.scss"
 
 function App() {
-  let absXStart
-  let absXEnd
-  const cards = [useRef(null), useRef(null), useRef(null)]
-  let activeCard = 0
-  const screenWidth = window.innerWidth
-
-  const scrollTo = (event) => {
-    absXEnd = event.changedTouches[0].screenX
-    if (absXEnd < absXStart) {
-      const diffX = absXStart - absXEnd
-      if (diffX > screenWidth / 2 && activeCard + 1 < cards.length) activeCard += 1
-    } else if (absXEnd > absXStart) {
-      const diffX = absXEnd - absXStart
-      if (diffX > screenWidth / 2 && activeCard > 0) activeCard += -1
-    }
-    console.log("activeCard: ", activeCard)
-    cards[activeCard].current.scrollIntoView({
-      behavior: 'smooth',
-      block: "center",
-      inline: "center"
-    })
-  }
 
   return (
-    <>
-      <div
-        className="container"
-        onTouchStart={(event) => {
-          absXStart = event.touches[0].screenX
-        }}
-        onTouchMove={(event) => {
-          //console.log(event.changedTouches[0].screenX)
-          //absX += event.touchList[0].clientX
-          //console.log(absX)
-          //console.log(event.touches[0].clientX)
-          //console.log(event.currentTarget())
-        }}
-        onTouchEnd={scrollTo}
-        onTouchCancel={scrollTo}
-      >
-        <div className="card" ref={cards[0]}>
-        </div>
-        <div className="card" ref={cards[1]}>
-        </div>
-        <div className="card" ref={cards[2]}>
-        </div>
-      </div>
+    <section className="wrapper">
 
-      <div className="abs-btn">
-        <button onClick={(e) => {
-          cards[1].current.scrollIntoView({
-            behavior: 'smooth',
-            block: "center",
-            inline: "center"
-          });
-        }
-        }>
-          Press me
-        </button>
-      </div>
-
-    </>
+      <main className="content">
+        <h1 className="title">X axis scroll</h1>
+        <ul className="scroll">
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0115.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_1151.jpg" alt="" title="" /></div>
+          </li>
+          <li className="item">
+            <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/44572/IMG_0121.jpg" alt="" title="" /></div>
+          </li>
+        </ul>
+      </main>
+    </section>
   );
 }
 
